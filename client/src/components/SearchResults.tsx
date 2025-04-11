9f40a46081a0a295cd13a77a748cbef09970b2c1
AnnuArchon <minastyr@gmail.com>
2025-04-11 16:43:52 -0400

    lots of making things pretty and routing updates

---
+++ client/src/components/SearchResults.tsx
@@ -0,0 +1,38 @@
+//import React from 'react'; not needed since this is a typescript file and typescript is crap
+
+const SearchResults = ({ results }: { results: any[] }) => {
+  return (
+    <div className="search-results">
+      <div className="row">
+        {results.map((book) => (
+          <div key={book.id} className="col-md-4 mb-4">
+            <div className="card h-100">
+              <img
+                src={book.volumeInfo.imageLinks?.thumbnail}
+                className="card-img-top"
+                alt={book.volumeInfo.title}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{book.volumeInfo.title}</h5>
+                <p className="card-text">
+                  <strong>Author(s):</strong> {book.volumeInfo.authors?.join(', ')}
+                </p>
+                <p className="card-text">{book.volumeInfo.description}</p>
+                <a
+                  href={book.volumeInfo.infoLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn btn-primary"
+                >
+                  View on Google Books
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SearchResults;
