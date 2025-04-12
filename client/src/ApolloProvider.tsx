c3f05438991a407c70fa71ccea1414751e932893
AnnuArchon <minastyr@gmail.com>
2025-04-12 16:40:21 -0400

    modified headers to dynamically add token

--- client/src/ApolloProvider.tsx
+++ client/src/ApolloProvider.tsx
@@ -1,16 +1,25 @@
 import { ReactNode } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider as Provider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-
-import { ApolloClient, InMemoryCache, ApolloProvider as Provider } from '@apollo/client';
-
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
-  headers: {
-    authorization: `Bearer ${localStorage.getItem('id_token') || ''}`,
-  },
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 interface ApolloProviderProps {
   children: ReactNode;
